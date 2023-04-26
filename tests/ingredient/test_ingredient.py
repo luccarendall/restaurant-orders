40b7166163aba37b25b9ef1d0e29cfd2f89326a1
LuccaRendall <luccarendall2@gmail.com>
2023-04-26 08:39:52 -0300

    Feat: Test_ingredient parcial

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,30 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_one = Ingredient("farinha")
+    ingredient_two = Ingredient("farinha")
+    ingredient_three = Ingredient("manteiga")
+
+    assert ingredient_one.name == "farinha"
+    assert ingredient_two.name == "farinha"
+    assert ingredient_three.name == "manteiga"
+
+    assert ingredient_one == ingredient_two
+    assert ingredient_one != ingredient_three
+    assert ingredient_two != ingredient_three
+
+    assert hash(ingredient_one) == hash(ingredient_two)
+    assert hash(ingredient_one) != hash(ingredient_three)
+    assert hash(ingredient_one) != hash(ingredient_three)
+    assert hash(ingredient_one) == hash(ingredient_two)
+
+    assert repr(ingredient_three) == "Ingredient('manteiga')"
+    assert repr(ingredient_three) == "Ingredient('farinha')"
+    assert repr(ingredient_two) == "Ingredient('farinha')"
+
+    assert ingredient_three.restrictions == {
+        Restriction.ANIMAL_DERIVED,
+        Restriction.LACTOSE
+    }
