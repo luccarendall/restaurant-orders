fbc07ea4e7cc09ee99c8fbfa4c9d3d3c8cdb1aa6
LuccaRendall <luccarendall2@gmail.com>
2023-04-26 17:31:16 -0300

    Feat: inventory_control - método check_recipe_avaliability

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -26,8 +26,11 @@ class InventoryMapping:
         self.inventory = read_csv_inventory(inventory_file_path)
 
     # Req 5.1
-    def check_recipe_availability(self, recipe: Recipe):
-        pass
+    def check_recipe_availability(self, dish: Recipe):
+        for ingredient in dish:
+            if dish[ingredient] > self.inventory[ingredient]:
+                return False
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
