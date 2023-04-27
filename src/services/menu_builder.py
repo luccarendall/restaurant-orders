793999a11da914f4a2cd0b7b5211b3f8878ba0ac
LuccaRendall <luccarendall2@gmail.com>
2023-04-27 07:34:52 -0300

    Feat: menu_builder falhando

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -24,6 +24,39 @@ class MenuBuilder:
 
         self.inventory.consume_recipe(curr_dish.recipe)
 
-    # Req 4
+    def check_recipe(self, recipe):
+        try:
+            if self.inventory.check_recipe_availability(recipe) is False:
+                return False
+            return True
+        except KeyError:
+            return False
+
+    # Req 4 e Req 6
     def get_main_menu(self, restriction=None) -> pd.DataFrame:
-        pass
+        data = []
+        menu_data_dishes = self.menu_data.dishes
+
+        for dish in menu_data_dishes:
+            recipe_info = {
+                "dish_name": "",
+                "ingredients": "",
+                "price": 0,
+                "restrictions": "",
+            }
+
+            recipe_info["restrictions"] = dish.get_restrictions()
+            if restriction in recipe_info["restrictions"]:
+                continue
+
+            if self.check_recipe(dish.recipe) is False:
+                continue
+
+            recipe_info["dish_name"] = dish.name
+            recipe_info["ingredients"] = dish.get_ingredients()
+            recipe_info["price"] = dish.price
+            recipe_info["restrictions"] = dish.restrictions
+            data.append(recipe_info)
+
+        data_dish = pd.DataFrame(data)
+        return data_dish
